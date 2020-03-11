bf47f4e6eea96e24042f982d25e41a9c63cb1628
bobrovnikovmr <bobrovnikovmr@gmail.com>
2020-03-11 13:38:13 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,45 @@
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QMessageBox>
+
+#include "window.h"
+
+int main (int argc, char *argv[])
+{
+  QApplication app (argc, argv);
+
+  QMainWindow *window = new QMainWindow;
+  QMenuBar *tool_bar = new QMenuBar (window);
+  Window *graph_area = new Window (window);
+  QAction *action;
+
+  if (graph_area->parse_command_line (argc, argv))
+    {
+      QMessageBox::warning (0, "Wrong input arguments!", 
+                            "Wrong input arguments!");
+      return -1;
+    }
+
+  action = tool_bar->addAction ("Change function", graph_area, SLOT (change_func ()));
+  action->setShortcut (QString ("Ctrl+C"));
+
+  action = tool_bar->addAction ("Exit", window, SLOT (close ()));
+  action->setShortcut (QString ("Ctrl+X"));
+
+  tool_bar->setMaximumHeight (30);
+
+  window->setMenuBar (tool_bar);
+  window->setCentralWidget (graph_area);
+  window->setWindowTitle ("Graph");
+
+  window->show ();
+  app.exec ();
+  delete graph_area;
+  delete tool_bar;
+  delete window;
+  return 0;
+}
